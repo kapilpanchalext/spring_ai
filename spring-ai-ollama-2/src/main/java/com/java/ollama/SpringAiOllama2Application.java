95d6189168f22ff64639e4968e3bdc5a01c93bc3
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-30 14:32:11 +0530

    [logic] added
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ spring-ai-ollama-2/src/main/java/com/java/ollama/SpringAiOllama2Application.java
@@ -0,0 +1,13 @@
+package com.java.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOllama2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOllama2Application.class, args);
+	}
+
+}
---
+++ spring-ai-ollama-2/src/test/java/com/java/ollama/SpringAiOllama2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOllama2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
